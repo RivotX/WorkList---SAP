f228496a56588734aba3339f4243417698927403
RivotX <miguelrosso60@gmail.com>
2024-04-05 11:41:34 +0200

    Add i18n translations for Clear Filters, Add Package, and Delete

--- webapp/Controller/Main.controller.js
+++ webapp/Controller/Main.controller.js
@@ -1,9 +1,12 @@
-sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
-	"use strict";
+sap.ui.define(
+	["./BaseController", "sap/m/MessageBox", "../model/formatter"],
+	function (BaseController, MessageBox, formatter) {
+		"use strict";
 
-	return BaseController.extend("com.myorg.myapp.controller.Main", {
-		sayHello: function () {
-			MessageBox.show("Hello World!");
-		}
-	});
-});
+		return BaseController.extend("com.myorg.myapp.controller.Main", {
+			sayHello: function () {
+				MessageBox.show("Hello World!");
+			},
+		});
+	}
+);
---
+++ webapp/Controller/TableControls.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+	"use strict";
+
+	return Controller.extend("webapp.Controller.TableControls", {
+		onInit: function () {
+			// This method is called upon initialization of the view.
+		},
+
+		onAfterRendering: function () {
+			// This method is called after the view has been rendered.
+		},
+
+		onExit: function () {
+			// This method is called when the view is about to be destroyed.
+		},
+	});
+});
