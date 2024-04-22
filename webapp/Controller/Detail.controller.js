5e6610c108b52c0712fc1458dd35199e2d61d266
RivotX <miguelrosso60@gmail.com>
2024-04-22 09:40:49 +0200

    Add file upload functionality and handle upload events in Detail controller and view

--- webapp/Controller/Detail.controller.js
+++ webapp/Controller/Detail.controller.js
@@ -26149,6 +26149,28 @@ sap.ui.define(
 				// Log the updated model
 				console.log(oDetailDataModel.getData());
 			},
+			handleUploadComplete: function(oEvent) {
+			var sResponse = oEvent.getParameter("response"),
+				aRegexResult = /\d{4}/.exec(sResponse),
+				iHttpStatusCode = aRegexResult && parseInt(aRegexResult[0]),
+				sMessage;
+
+			if (sResponse) {
+				sMessage = iHttpStatusCode === 200 ? sResponse + " (Upload Success)" : sResponse + " (Upload Error)";
+				MessageToast.show(sMessage);
+			}
+		},
+
+		handleUploadPress: function() {
+			var oFileUploader = this.byId("fileUploader");
+			oFileUploader.checkFileReadable().then(function() {
+				oFileUploader.upload();
+			}, function(error) {
+				MessageToast.show("The file cannot be read. It may have changed.");
+			}).then(function() {
+				oFileUploader.clear();
+			});
+		}
 		});
 	}
 );
